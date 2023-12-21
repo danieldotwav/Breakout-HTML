85bda1b42c5737399553acce93e3c4ee07d71100
Daniel Rivas <drivas8891@gmail.com>
2023-12-20 17:16:25 -0800

    Added powerup effects

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const BRICK_HEIGHT = 16;
 const PADDLE_WIDTH = BRICK_WIDTH * 2;
 const PADDLE_HEIGHT = BRICK_HEIGHT / 2;
 
+const ORIGINAL_BALL_SIZE = 5;
+
 const bricks = [];
 
 const canvas = document.getElementById('game');
@@ -70,12 +72,16 @@ const ball = {
 /// MUTATORS AND ACCESSORS
 ////////////////////////////
 
+const ORIGINAL_BALL_SPEED = ball.speed;
+const ORIGINAL_PADDLE_WIDTH = paddle.width;
+
 function setPaddlePosition(horizontalPosition, verticalPosition) {
     paddle.x = horizontalPosition;
     paddle.y = verticalPosition;
 }
 
 function setPaddleWidth(newWidth) { paddle.width = newWidth; }
+function getOriginalPaddleWidth() { return ORIGINAL_PADDLE_WIDTH; }
 
 
 function setBallPosition(horizontalPosition, verticalPosition) {
@@ -83,7 +89,21 @@ function setBallPosition(horizontalPosition, verticalPosition) {
     ball.y = verticalPosition;
 }
 
-function setBallSpeed(newSpeed) { ball.speed = newSpeed; }
+function setBallSpeed(newSpeed) {
+    let ratioX = ball.dx / ball.speed;
+    let ratioY = ball.dy / ball.speed;
+
+    ball.speed = newSpeed;
+
+    // Adjust dx and dy based on the new speed while maintaining direction
+    ball.dx = ratioX * ball.speed;
+    ball.dy = ratioY * ball.speed;
+}
+
+function setBallSize(newSize) {
+    ball.height = newSize;
+    ball.width = newSize;
+}
 
 
 /////////////////////////////////
@@ -106,7 +126,7 @@ function createGameGrid() {
     }
 }
 
-// Checks for collision between two objects using axis-aligned bounding box (AABB)
+// Check for collision between two objects using axis-aligned bounding box (AABB)
 function collides(obj1, obj2) {
     return obj1.x < obj2.x + obj2.width &&
         obj1.x + obj1.width > obj2.x &&
@@ -128,8 +148,8 @@ function updateBallSpeed() {
     const speedIncreaseFactor = 1.1;
     ball.speed *= speedIncreaseFactor;
 
-    // Update the ball's velocity components (dx and dy) based on the new speed
-    // Preserve the direction of the ball while updating its speed
+    // Update the ball's velocity components based on the new speed
+    // Preserve the direction of the ball while updating speed
     ball.dx = (ball.dx / Math.abs(ball.dx)) * ball.speed;
     ball.dy = (ball.dy / Math.abs(ball.dy)) * ball.speed;
 }
@@ -162,32 +182,30 @@ function resetBall() {
     ball.dy = -ball.speed; // Always move up
 }
 
-function resetGame() {
-    // Reset Score
-    score = 0;
-    updateScore();
-
-    // Reset the game over flag
-    isGameOver = false;
-
-    // Hide the Game Over Text
-    hideGameOver();
-    hideWinScreen();
-
-    // Repopulate the bricks array for a new game
-    bricks.length = 0; // Clear the existing bricks;
-    createGameGrid(); 
-
-    // Reset Ball Position/Velocity
-    resetBall();
-
-    // Restart the game loop
-    requestAnimationFrame(loop);
+function resetPaddle() {
+    paddle.x = canvas.width / 2 - PADDLE_WIDTH / 2;
+    paddle.y = 440;
+    width: PADDLE_WIDTH;
+    height: PADDLE_HEIGHT;
+    dx: 0;
 }
+
 
 //////////////////////////////
 /// GAME LOOP
 /////////////////////////
+
+// Global flags for each powerup
+let slowBallActive = false;
+let widePaddleActive = false;
+let powerupActive = false;
+let bigBallActive = false;
+
+let widePaddleDuration = 0;
+let slowBallDuration = 0;
+let bigBallDuration = 0;
+
+let previousBallSpeed = 0;
 
 let score = 0;
 let isGameOver = false;
@@ -200,10 +218,8 @@ function loop() {
         requestAnimationFrame(loop);
         context.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Move Paddle
+        // Move Paddle and Paddle
         movePaddle();
-
-        // Move Ball
         moveBall()
 
         // Check Ball Collision
@@ -240,6 +256,16 @@ function loop() {
 //////////////////////////////
 /// GAME FUNCTIONS
 /////////////////////////
+
+const POWERUP_CHANCE = 0.10; // 10% chance for powerup
+const POWERUP_DURATION = 7;
+
+// Add: 1. AI OVERRIDE 2. BIG BALL
+const POWERUP_TYPES = {
+    WIDER_BAR: 'wider_bar',
+    SLOW_BALL: 'slow_ball',
+    BIG_BALL: 'big_ball'
+}
 
 function movePaddle() {
     paddle.x += paddle.dx;
@@ -280,8 +306,9 @@ function checkBallPaddleCollision() {
 }
 
 function checkBallBrickCollision() {
-    // If ball collides with a brick, remove the brick and change the ball velocity based on the side the brick was hit on
-    for (let i = 0; i < bricks.length; i++) {
+    let collisionOccurred = false;
+
+    for (let i = 0; i < bricks.length && !collisionOccurred; i++) {
         const brick = bricks[i];
 
         if (collides(ball, brick)) {
@@ -292,21 +319,112 @@ function checkBallBrickCollision() {
             score++;
             updateScore();
 
-            // Above or Below the brick, change y velocity
-            // Account for the balls speed since it will be inside the brick when it collides
-            if (ball.y + ball.height - ball.speed <= brick.y ||
-                ball.y >= brick.y + brick.height - ball.speed) {
+            // Update Powerup Gauge
+            updatePowerupGauge();
+
+            // Check For Powerup Chance
+            checkForRandomPowerupChance();
+
+            // Calculate where the ball hit the brick
+            const ballBottom = ball.y + ball.height;
+            const ballTop = ball.y;
+            const brickBottom = brick.y + brick.height;
+            const brickTop = brick.y;
+            const ballCenterX = ball.x + ball.width / 2;
+            const ballCenterY = ball.y + ball.height / 2;
+            const brickCenterX = brick.x + brick.width / 2;
+            const brickCenterY = brick.y + brick.height / 2;
+
+            // Determine the side of the collision
+            const hitVertical = Math.abs(ballCenterX - brickCenterX) < brick.width / 2;
+            const hitHorizontal = Math.abs(ballCenterY - brickCenterY) < brick.height / 2;
+
+            // Ball hits the brick from the bottom or top
+            if (hitVertical && !hitHorizontal) {
                 ball.dy *= -1;
             }
-            // ball is on either side of the brick, change x velocity
-            else {
+            // Ball hits the brick from the side
+            else if (hitHorizontal && !hitVertical) {
                 ball.dx *= -1;
             }
-            break;
+            // Corner hit, reflect both axes
+            else {
+                ball.dx *= -1;
+                ball.dy *= -1;
+            }
+            collisionOccurred = true;
         }
     }
 }
 
+function getRandomPowerup() {
+    const keys = Object.keys(POWERUP_TYPES);
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    return POWERUP_TYPES[keys[randomIndex]];
+}
+
+function displayPowerupText(text) {
+    const powerupTextElement = document.getElementById('powerupText');
+    powerupTextElement.textContent = text;
+    powerupTextElement.style.display = 'block';
+    setTimeout(() => powerupTextElement.style.display = 'none', 5000); // Hide the text after 5 seconds
+}
+
+function checkForRandomPowerupChance() {
+    if (Math.random() < POWERUP_CHANCE && !powerupActive) {
+        powerupActive = true; // Global flag indicating a powerup is active
+        let selectedPowerup = getRandomPowerup();
+
+        // Activate the selected powerup and set the respective flag
+        if (selectedPowerup === POWERUP_TYPES.SLOW_BALL && !slowBallActive) {
+            slowBallDuration = 3;
+            previousBallSpeed = ball.speed;
+            setBallSpeed(ball.speed / 2);
+            slowBallActive = true; // Set the slow ball flag
+            displayPowerupText('SLOWWWW');
+        }
+        else if (selectedPowerup === POWERUP_TYPES.WIDER_BAR && !widePaddleActive) {
+            widePaddleDuration = POWERUP_DURATION;
+            setPaddleWidth(paddle.width * 2);
+            widePaddleActive = true; // Set the wide paddle flag
+            displayPowerupText('WIDE PADDLE');
+        }
+        else if (selectedPowerup === POWERUP_TYPES.BIG_BALL && !bigBallActive) {
+            bigBallDuration = POWERUP_DURATION;
+            setBallSize(12);
+            bigBallActive = true; // Set the wide paddle flag
+            displayPowerupText('BIG BALL');
+        }
+    }
+}
+
+function updatePowerupGauge() {
+    if (widePaddleDuration > 0) {
+        --widePaddleDuration;
+        if (widePaddleDuration === 0) {
+            setPaddleWidth(getOriginalPaddleWidth()); // Reset paddle width
+            widePaddleActive = false;
+        }
+    }
+
+    if (slowBallDuration > 0) {
+        --slowBallDuration;
+        if (slowBallDuration === 0) {
+            setBallSpeed(ORIGINAL_BALL_SPEED); // Reset ball speed
+            slowBallActive = false;
+        }
+    }
+
+    if (bigBallDuration > 0) {
+        --bigBallDuration;
+        if (bigBallDuration === 0) {
+            setBallSize(ORIGINAL_BALL_SIZE); // Reset ball speed
+            bigBallActive = false;
+        }
+    }
+
+    powerupActive = widePaddleActive || slowBallActive || bigBallActive;
+}
 function drawWalls() {
     context.fillStyle = colorMap['P'];
     context.fillRect(0, 0, canvas.width, WALL_WIDTH);
@@ -332,6 +450,73 @@ function drawPaddle() {
     context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
 }
 
+function resetGame() {
+    // Reset Score
+    score = 0;
+    updateScore();
+
+    // Reset the game state flags
+    isGameOver = false;
+    slowBallActive = false;
+    widePaddleActive = false;
+    powerupActive = false;
+    widePaddleDuration = 0;
+    slowBallDuration = 0;
+
+    // Reset any powerups if active
+    setBallSpeed(ORIGINAL_BALL_SPEED);
+    setPaddleWidth(ORIGINAL_PADDLE_WIDTH);
+
+    // Hide the Game Over Text
+    hideGameOver();
+    hideWinScreen();
+
+    // Repopulate the bricks array for a new game
+    bricks.length = 0; // Clear the existing bricks;
+    createGameGrid();
+
+    // Reset Ball Position/Velocity
+    resetBall();
+    resetPaddle();
+
+    // Restart the game loop
+    requestAnimationFrame(loop);
+}
+
+
+//////////////////////////////
+/// UTILITY
+/////////////////////////
+
+let lastFrameTime = 0;
+let frameCount = 0;
+let fps = 0;
+let lastSecond = Date.now();
+
+function animate(currentTime) {
+    // Calculate the time difference
+    let deltaTime = currentTime - lastFrameTime;
+    lastFrameTime = currentTime;
+
+    // Increment the frame count
+    frameCount++;
+
+    // Update the FPS every second
+    if (currentTime - lastSecond >= 1000) {
+        fps = frameCount;
+        frameCount = 0;
+        lastSecond = currentTime;
+
+        // Update FPS display on the page
+        document.getElementById('fpsCounter').innerText = `FPS: ${fps}`;
+    }
+
+    // Request the next frame
+    requestAnimationFrame(animate);
+}
+
+// Start the animation
+//requestAnimationFrame(animate);
 
 //////////////////////////////
 /// DIRECTIONAL MOTION
